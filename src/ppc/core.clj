236dcf47d59ba5487d295ceb1c69db98ce4a3fe4
Steen Larsen <steen666@gmail.com>
2012-03-29 15:26:27 +0200

    plans, services and prices

--- src/ppc/core.clj
+++ src/ppc/core.clj
@@ -1,10 +1,48 @@
 (ns ppc.core
   (:use dynamo4clj.core))
 
+(defrecord Pris [kontrakt varenr pris copydan koda radio-koda])
+
+(defrecord Produkt [varenr navn fo pl pgt pg vaegt sorteringsgruppe sortering salgsstart salgsslut services])
+
+(defrecord Service [type id provisionerings-kode logistik-kode])
+
 (defn find-pris [kontrakt & varenr]
   (if-not (empty? varenr)
     (find-items "priser" kontrakt false ["eq" (first varenr)])
     (find-items "priser" kontrakt false)))
 
-(defn gem-pris [kontrakt varenr navn pl pgt pg]
-  (insert-item "priser" {:kontrakt kontrakt :varenr varenr :navn navn :pg pg :pl pl :pgt pgt}))
+(defn gem-pris [pris]  
+  (try
+    (let [tpris {:kontrakt (:kontrakt pris) :varenr (:varenr pris) :pris (:pris pris) :copydan (:copydan pris)
+                :koda (:koda pris) :radio-koda (:radio-koda pris) :moms (str (* 0.25 (Double/parseDouble (:pris pris))))
+                :totalpris (str (+ (Double/parseDouble (:pris pris)) (Double/parseDouble (:copydan pris)) (Double/parseDouble (:koda pris)) (Double/parseDouble (:radio-koda pris)) (* 0.25 (Double/parseDouble (:pris pris)))))}]    
+      (insert-item "priser" tpris)
+      tpris)
+    (catch Exception e
+      nil)))
+
+(defn gem-produkt [produkt]
+  (try
+    (insert-item "produkter" produkt)
+    produkt
+    (catch Exception e
+      nil)))
+
+(defn find-produkt [varenr]
+  (get-item "produkter" varenr))
+
+(defn find-alle-produkter []
+  (scan "produkter"))
+
+(defn gem-service [service]
+  (try
+    (insert-item "services" service)
+    service
+    (catch Exception e
+      nil)))
+
+(defn find-service [type & id]
+  (if-not (empty? id)
+    (find-items "services" type false ["eq" (first id)])
+    (find-items "services" type false)))
--- src/ppc/routes.clj
+++ src/ppc/routes.clj
@@ -10,15 +10,41 @@
             [clojure.data.json :as json]))
 
 (defroutes handler
+  (GET ["/:context/produkter" , :context #".[^/]*"] []       
+       (json-response (find-alle-produkter) "application/json"))
+
+  (GET ["/:context/produkter/:varenr" , :context #".[^/]*"] [varenr]       
+       (json-response (find-produkt (Integer/parseInt varenr)) "application/json"))
+  
   (GET ["/:context/prisplan/:kontrakt" , :context #".[^/]*"] [kontrakt]       
        (json-response (find-pris kontrakt) "application/json"))
   
   (GET ["/:context/prisplan/:kontrakt/:varenr" , :context #".[^/]*"] [kontrakt varenr]
        (json-response (find-pris kontrakt (Integer/parseInt varenr)) "application/json"))
+
+  (GET ["/:context/services/:type" , :context #".[^/]*"] [type]       
+       (json-response (find-service type) "application/json"))
+  
+  (GET ["/:context/services/:type/:id" , :context #".[^/]*"] [type id]
+       (json-response (find-service type id) "application/json"))
+
+  (POST ["/:context/services" , :context #".[^/]*"] req        
+        (let [res (gem-service (parse-body (:body req)))]
+          (if (nil? res)
+            (json-response nil "application/json" :status 400)
+            (json-response res "application/json"))))
+
+  (POST ["/:context/produkter" , :context #".[^/]*"] req        
+        (let [res (gem-produkt (parse-body (:body req)))]
+          (if (nil? res)
+            (json-response nil "application/json" :status 400)
+            (json-response res "application/json"))))
   
   (POST ["/:context/prisplan" , :context #".[^/]*"] req        
-        (let [res (parse-body (:body req))]
-          (json-response res "application/json"))))
+        (let [res (gem-pris (parse-body (:body req)))]
+          (if (nil? res)
+            (json-response nil "application/json" :status 400)
+            (json-response res "application/json")))))
         
 (def app
   (-> (handler/site handler)
